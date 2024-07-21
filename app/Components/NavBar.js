b395eab770d719f11d818778aa4994e12d38df95
migougou <miguel.litche@espi.fr>
2024-07-21 17:58:23 +0200

    Ajout de Heroicons / ajout des deux icons du navbar

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { MoonIcon, UserIcon } from '@heroicons/react/24/solid';
 
 export default function NavBar(){
   return (
@@ -16,6 +17,7 @@ export default function NavBar(){
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
               </svg>
             </button>
+            <div className="flex items-center space-x-4">
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
@@ -40,6 +42,9 @@ export default function NavBar(){
                   <Link href="/ranges" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ranges</Link>
                 </li>
               </ul>
+            </div>
+            <MoonIcon className="h-6 w-6 text-gray-600 dark:text-gray-300" />
+            <UserIcon className="h-6 w-6 text-gray-600 dark:text-gray-300" />
             </div>
           </div>
         </nav>
