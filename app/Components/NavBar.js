d17a36f7b75061707a16a0e5d99c47c729b8b290
migougou <miguel.litche@espi.fr>
2024-07-23 19:22:41 +0200

    Création de la page contact

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -25,9 +25,6 @@ export default function NavBar() {
                   <Link href="/outils" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Outils</Link>
                 </li>
                 <li>
-                  <Link href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
-                </li>
-                <li>
                   <Link href="/videos" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Vidéos</Link>
                 </li>
                 <li>
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,36 +1,60 @@
 
 
-export default function contact(){
-    return (
-  
-  
-  <form className="max-w-md mx-auto mt-20">
-    <div className="relative z-0 w-full mb-5 group">
-        <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-        <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
-    </div>
-    <div className="grid md:grid-cols-2 md:gap-6">
-      <div className="relative z-0 w-full mb-5 group">
-          <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-          <label for="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Prénom</label>
+export default function contact() {
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-gray-10 p-16">
+      <div className="w-full max-w-4xl bg-white border border-gray-300 shadow-lg rounded-t-md">
+        <div className="p-10">
+          <div className="flex flex-wrap -mx-8">
+            <div className="w-full lg:w-1/2 px-8">
+              <h1 className="text-xl font-semibold text-gray-900 mb-4">Me contacter</h1>
+              <p className="text-gray-600 mb-6">
+                Vous avez des questions ou vous voulez en savoir plus?<br /> N'hésitez pas à me contacter en utilisant le formulaire ci-contre.<br /> Je vous répondrai dès que possible!
+              </p>
+              <h1 className="text-xl font-semibold text-gray-900 mb-4">Mais aussi</h1>
+              <p className="text-gray-600 mb-6">
+                Tu peux aussi me contacter via ces réseaux sociaux
+              </p>
+              <div className="flex space-x-4 mb-6">
+                <a href="#" aria-label="Instagram">
+                  <img src="images/instagram.png" alt="Instagram" className="w-14 h-14" />
+                </a>
+                <a href="#" aria-label="Facebook">
+                  <img src="images/facebook.png" alt="Facebook" className="w-14 h-14" />
+                </a>
+                <a href="#" aria-label="Discord">
+                  <img src="images/discord.png" alt="Discord" className="w-14 h-14" />
+                </a>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 px-8">
+              <form className="space-y-6">
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <input type="text" id="prenom" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required placeholder="Prénom" />
+                  </div>
+                  <div>
+                    <input type="text" id="nom" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required placeholder="Nom" />
+                  </div>
+                </div>
+                <div>
+                  <input type="text" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required placeholder="E-mail" />
+                </div>
+                <div>
+                  <input type="email" id="email" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required placeholder="Objet" />
+                </div>
+                <div>
+                  <textarea id="message" rows="4" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required placeholder="Message"></textarea>
+                </div>
+                <div className="flex justify-center">
+                  <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Envoyer mon message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <img src="images/banniere.png" alt="Bannière" className="w-full h-24  object-fit: fill rounded-b-md" />
       </div>
-      <div className="relative z-0 w-full mb-5 group">
-          <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-          <label for="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom</label>
-      </div>
     </div>
-    <div className="relative z-0 w-full mb-5 group">
-        <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-        <label for="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mot de passe</label>
-    </div>
-    <div className="relative z-0 w-full mb-5 group">
-        <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-        <label for="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirmer mot de passe</label>
-    </div>
-    <div  className="flex justify-center">
-    <button type="submit" className="text-white bg-yellow-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">S'inscrire</button>
-    </div>
-  </form>
-  
-    )
-  }
+  );
+}
