6534f8a2792ce13043969709581d8bdbfd162035
migougou <miguel.litche@espi.fr>
2024-07-27 18:58:55 +0200

    push

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,14 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import './globals.css';
 import WarningBanner from './components/WarningBanner';
+import { ThemeProvider } from 'next-themes';
+
+
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+      <html lang="en"  suppressHydrationWarning>
+        <ThemeProvider enableSystem={true} attribute='class'> 
       <body>
         <div className="flex flex-col min-h-screen">
           <WarningBanner />
@@ -16,6 +20,8 @@ export default function RootLayout({ children }) {
           <Footer /> 
         </div>
       </body>
-    </html>
+      </ThemeProvider>   
+    </html> 
+
   );
 }
