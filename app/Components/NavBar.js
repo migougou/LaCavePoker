34e2bf5ee2caf1270312d18392a052b8611c9a5a
migougou <miguel.litche@espi.fr>
2024-08-11 17:05:47 +0200

    supp ThemeProvider du NavBar

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { MoonIcon, UserIcon } from '@heroicons/react/24/solid';
-import ThemeSwitcher from './ThemeSwitcher';
-import { ThemeProvider } from 'next-themes';
+
 
 export default function NavBar() {
 
@@ -34,9 +33,6 @@ export default function NavBar() {
                 </li>
                 <li>
                   <Link href="/ranges" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ranges</Link>
-                </li>
-                <li>
-                  <ThemeProvider />
                 </li>
                 <li>
                   <MoonIcon className="h-6 w-6 text-gray-600 dark:text-gray-300" />
