c0ed29d2e572b80f3501cba32447ed5e863b523f
migougou <miguel.litche@espi.fr>
2024-08-12 13:36:01 +0200

    Mise en place correct des cards

--- app/coaching/components/cardPrice.js
+++ app/coaching/components/cardPrice.js
@@ -1,68 +1,60 @@
 'use client';
 
-
-
-export default function CarPrice({title, price, teamMembers, storage, integrationHelp }) {
-
+export default function CarPrice({
+    title,
+    price,
+    duration,
+    gameplay,
+    feedback,
+    accessRanges,
+    accessVideos
+}) {
 
 
     return (
-
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{title}</h5>
+        <div className="w-full max-w-xs bg-white border border-black rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-white">{title}</h5>
             <div className="flex items-baseline text-gray-900 dark:text-white">
-                <span className="text-3xl font-semibold">€</span>
                 <span className="text-5xl font-extrabold tracking-tight">{price}</span>
-                <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+                <span className="text-3xl font-semibold">€</span>
             </div>
-            {teamMembers && (
-            <ul role="list" class="space-y-5 my-7">
-                <li class="flex items-center">
-                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <ul role="list" className="space-y-5 my-7">
+                <li className="flex items-center">
+                    <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">2 team members</span>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{duration}</span>
                 </li>
-                <li class="flex">
-                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <li className="flex">
+                    <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">20GB Cloud storage</span>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{gameplay}</span>
                 </li>
-                <li class="flex">
-                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <li className="flex">
+                    <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Integration help</span>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{feedback}</span>
                 </li>
-                <li class="flex line-through decoration-gray-500">
-                    <svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 ms-3">Sketch Files</span>
-                </li>
-                <li class="flex line-through decoration-gray-500">
-                    <svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 ms-3">API Access</span>
-                </li>
-                <li class="flex line-through decoration-gray-500">
-                    <svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 ms-3">Complete documentation</span>
-                </li>
-                <li class="flex line-through decoration-gray-500">
-                    <svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 ms-3">24×7 phone & email support</span>
-                </li>
+                {accessRanges && (
+                    <li className="flex">
+                        <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux ranges</span>
+                    </li>
+                )}
+                {accessVideos && (
+                    <li className="flex">
+                        <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux vidéos</span>
+                    </li>
+                )}
             </ul>
-        )}
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choose plan</button>
         </div>
 
-);
+    );
 }
--- app/coaching/page.js
+++ app/coaching/page.js
@@ -8,16 +8,48 @@ export default function Coaching() {
 
 return (
     <div className="py-10 text-center">
-        <div className="py-10 max-w-4xl mx-auto">
-            <h1 className="text-4xl font-bold mb-3 pb-16">COACHING</h1>
-            <p className="mb-5">Voici un choix de coaching que je vous propose.Vous pouvez me poser des questions sur n’importe quelle offre, en m’écrivant dans contact ou via mes réseaux.</p>
-        </div>
-        <div className="flex justify-around">
-                <CardPrice title="LinkFinder" price="30" teamMembers="2" storage="20GB" integrationHelp={true} />
-                <CardPrice title="Premium plan" price="99" teamMembers="5" storage="50GB" integrationHelp={true} />
-                <CardPrice title="Enterprise plan" price="199" teamMembers="10" storage="100GB" integrationHelp={true} />
-                <CardPrice title="Custom plan" price="Contact us" />
-            </div>
+    <div className="py-10 max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold mb-3 pb-16">COACHING</h1>
+        <p className="mb-5">Voici un choix de coaching que je vous propose. Vous pouvez me poser des questions sur n’importe quelle offre, en m’écrivant dans contact ou via mes réseaux.</p>
     </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-items-center px-20">
+        <CardPrice
+            title="LinkFinder"
+            price="30"
+            duration="1H00-1H30"
+            gameplay="Venir avec un gameplay de 20 minutes en 2 tables max"
+            feedback="Retour constructif sur les points à travailler"
+            accessRanges={true}
+            accessVideos={true}
+        />
+        <CardPrice
+            title="Pack 10 heures"
+            price="400"
+            duration="10 heures de coaching"
+            gameplay="Jeu 3"
+            feedback="Retour par DM sur les mains jouées"
+            accessRanges={true}
+            accessVideos={true}
+        />
+        <CardPrice
+            title="Pack 20 heures"
+            price="800"
+            duration="20 heures de coaching"
+            gameplay="Jeu 6"
+            feedback="Réduction sur les cours privés"
+            accessRanges={true}
+            accessVideos={true}
+        />
+        <CardPrice
+            title="Coaching for Profit"
+            price="Contact us"
+            duration="Coaching sur mesure"
+            gameplay="Accès à un groupe privé Facebook"
+            feedback="Support personnalisé par email"
+            accessRanges={true}
+            accessVideos={true}
+        />
+    </div>
+</div>
 );
 }
