525576b14b426be9efab02b5ac584b7340fb485f
migougou <miguel.litche@espi.fr>
2024-08-21 16:34:11 +0200

    Responsive du NavBar, ainsi qu'une logique pour changer l'icone de darkmode.

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -1,21 +1,39 @@
 "use client"
 import Link from 'next/link';
-import { MoonIcon, UserIcon } from '@heroicons/react/24/solid';
+import { MoonIcon, UserIcon, Bars3Icon, SunIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
 
 export default function NavBar({ dark, toggleDarkMode }) {
+  const [isOpen, setIsOpen] = useState(false); // Ajouter un état pour le menu déroulant
+
   return (
     <div className={dark ? "dark" : ""}>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="flex flex-wrap items-center justify-between p-4 px-10">
+        <div className="p-4 px-10 flex flex-wrap items-center justify-between">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <Link href="/">
               <img src="images/logo 2.png" className="h-12" alt="La Cave Poker Logo" />
             </Link>
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">La Cave Poker</span>
           </div>
-          <div className="flex items-center space-x-4">
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div className="flex items-center"></div>
+          <div className="flex items-center md:hidden">
+                      {/* Bouton toggle et icônes à côté seulement sur petit écran */}
+                      <div onClick={toggleDarkMode} className="cursor-pointer mr-4 md:hidden">
+              {dark ? (
+                <SunIcon className="h-6 w-6 text-gray-900 dark:text-white" />
+              ) : (
+                <MoonIcon className="h-6 w-6 text-gray-900 dark:text-white" />
+              )}
+            </div>
+          <UserIcon className="h-6 w-6 mr-4 text-gray-900 dark:text-white" />
+          
+          <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+              <Bars3Icon className="h-6 w-6 text-gray-900 dark:text-white" />
+            </button>
+            </div>
+            <div className={`w-full md:flex md:items-center md:w-auto ${isOpen ? 'block' : 'hidden'} text-right`} id="navbar-default">
+              <ul className="flex flex-col md:flex-row md:space-x-8 rtl:md:space-x-reverse mt-4 md:mt-0 bg-gray-50 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                   <Link href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Accueil</Link>
                 </li>
@@ -34,15 +52,18 @@ export default function NavBar({ dark, toggleDarkMode }) {
                 <li>
                   <Link href="/ranges" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ranges</Link>
                 </li>
-                <li>
-                  <MoonIcon onClick={toggleDarkMode} className="h-6 w-6 text-gray-900 dark:text-white cursor-pointer" />
+                <li onClick={toggleDarkMode} className="hidden md:block cursor-pointer">
+                  {dark ? (
+                    <SunIcon className="h-6 w-6 text-gray-900 dark:text-white" />
+                  ) : (
+                    <MoonIcon className="h-6 w-6 text-gray-900 dark:text-white" />
+                  )}
                 </li>
-                <li>
+                <li className="hidden md:block">
                   <UserIcon className="h-6 w-6 text-gray-900 dark:text-white" />
                 </li>
               </ul>
             </div>
-          </div>
         </div>
         <div className="px-32">
           <hr className="my-6 border-stone-950 sm:mx-auto dark:border-gray-700 lg:my-0" />
