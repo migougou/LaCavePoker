8af1d69379f0f626ad9633fefb577162173d4e5a
migougou <miguel.litche@espi.fr>
2024-08-21 04:11:12 +0200

    test d'appelle d'image venant de la database

--- app/page.js
+++ app/page.js
@@ -4,52 +4,44 @@ import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 export default function Accueil() {
-
     const supabase = createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     );
 
-    const [contacts, setContacts] = useState([]);
+    const [tools, setTools] = useState([]);
+
     async function fetchData() {
-        const { data, error } = await supabase.from("Contact").select("*");
+        const { data, error } = await supabase.from('Outils').select('*');
         if (error) {
             console.error('Error fetching data:', error);
             return;
         }
-
-        setContacts(data);
+        setTools(data);
     }
+
     useEffect(() => {
         fetchData();
-    }, []
-    );
-
-
+    }, []);
 
     return (
-
         <div>
-            <div>
             <h1 className="mt-10 text-3xl font-bold">Contacts List</h1>
-            {contacts.length > 0 ? (
+            {tools.length > 0 ? (
                 <ul className="mt-4 space-y-4">
-                    {contacts.map((contact) => (
-                        <li key={contact.id} className="border p-4 rounded-lg shadow">
-                            <p>First Name: {contact.firstName}</p>
-                            <p>Last Name: {contact.lastName}</p>
-                            <p>Email: {contact.email}</p>
-                            <p>Sujet: {contact.subject}</p>
-                            <p>Message: {contact.message}</p>
+                    {tools.map((tool) => (
+                        <li key={tool.id} className="border p-4 rounded-lg shadow">
+                            <p>Last Name: {tool.name}</p>
+                            <p>First Name: {tool.description}</p>
+                            {tool.imageURL && (
+                                <img src={tool.imageURL} alt={`Image de ${tool.name}`} className="mt-2 max-w-full" />
+                            )}
                         </li>
                     ))}
                 </ul>
             ) : (
                 <p>No contacts found.</p>
             )}
-            </div>
         </div>
-        
-
-    )
+    );
 }
