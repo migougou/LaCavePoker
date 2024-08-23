d6c084a1a71d4d732c98e4a3dc51afbd1616f29b
migougou <miguel.litche@espi.fr>
2024-08-23 21:40:30 +0200

    MAJ du component CardPrice en mode dynamique (ok) continuer à travailler sur la page.

--- app/coaching/components/cardPrice.js
+++ app/coaching/components/cardPrice.js
@@ -1,78 +1,76 @@
 'use client';
 
-export default function CarPrice({
-    title,
-    price,
-    duration,
-    gameplay,
-    feedback,
-    accessGroupe,
-    accessDiscord,
-    accessRanges,
-    accessVideos
-}) {
+import useSupabaseTable from '../../hooks/useSupabaseTable';
 
+export default function CardPrice() {
+    const { data: cardPrices, loading, error } = useSupabaseTable('CardPrice');
+
+    if (loading) return <p>Chargement...</p>;
+    if (error) return <p>Erreur : {error}</p>;
 
     return (
-        <div className="w-full max-w-xs bg-white border border-black rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-white">{title}</h5>
-            <div className="flex items-baseline text-gray-900 dark:text-white">
-                <span className="text-5xl font-extrabold tracking-tight">{price}</span>
-                <span className="text-3xl font-semibold">€</span>
-            </div>
-            <ul role="list" className="space-y-5 my-7">
-                <li className="flex items-center">
-                    <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{duration}</span>
-                </li>
-                <li className="flex">
-                    <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{gameplay}</span>
-                </li>
-                <li className="flex">
-                    <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{feedback}</span>
-                </li>
-                {accessDiscord && (
-                    <li className="flex">
-                        <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                        </svg>
-                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès à un discord privé</span>
-                    </li>
-                )}
-                {accessGroupe && (
-                    <li className="flex">
-                        <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                        </svg>
-                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès à 2H de coaching groupé par mois</span>
-                    </li>
-                )}
-                {accessRanges && (
-                    <li className="flex">
-                        <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                        </svg>
-                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux ranges</span>
-                    </li>
-                )}
-                {accessVideos && (
-                    <li className="flex">
-                        <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                        </svg>
-                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux vidéos</span>
-                    </li>
-                )}
-            </ul>
+        <div className="flex flex-wrap justify-center gap-12 ">
+            {cardPrices.map((card) => (
+                <div key={card.id} className="max-w-xs bg-white border border-black rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-white">{card.pack}</h5>
+                    <div className="flex items-baseline text-gray-900 dark:text-white">
+                        <span className="text-5xl font-extrabold tracking-tight">{card.price}</span>
+                        <span className="text-3xl font-semibold">€</span>
+                    </div>
+                    <ul role="list" className="space-y-5 my-7">
+                        <li className="flex items-center">
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                            </svg>
+                            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{card.time}</span>
+                        </li>
+                        <li className="flex">
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                            </svg>
+                            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{card.line1}</span>
+                        </li>
+                        <li className="flex">
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                            </svg>
+                            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{card.line2}</span>
+                        </li>
+                        {card.accessDiscord && (
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès à un discord privé</span>
+                            </li>
+                        )}
+                        {card.accessGroupe && (
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès à 2H de coaching groupé par mois</span>
+                            </li>
+                        )}
+                        {card.accessRanges && (
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux ranges</span>
+                            </li>
+                        )}
+                        {card.accessVideos && (
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux vidéos</span>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            ))}
         </div>
-
     );
 }
--- app/coaching/page.js
+++ app/coaching/page.js
@@ -1,63 +1,16 @@
 'use client';
-import CardPrice from "./components/cardPrice"
-
+import CardPrice from "./components/cardPrice";
 
 export default function Coaching() {
-
-
-
-return (
-    <div className="py-10 text-center">
-    <div className="py-10 max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-3 pb-16">COACHING</h1>
-        <p className="mb-5">Voici un choix de coaching que je vous propose. Vous pouvez me poser des questions sur n’importe quelle offre, en m’écrivant dans contact ou via mes réseaux.</p>
-    </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-items-center px-20">
-        <CardPrice
-            title="LinkFinder"
-            price="30"
-            duration="1H00-1H30"
-            gameplay="Venir avec un gameplay de 20 minutes en 2 tables max"
-            feedback="Retour constructif sur les points à travailler"
-            accessDiscord={true}
-            accessGroupe={true}
-            accessRanges={true}
-            accessVideos={true}
-        />
-        <CardPrice
-            title="Pack 10 heures"
-            price="500"
-            duration="1H à 2H / semaine"
-            gameplay=""
-            feedback=""
-            accessDiscord={true}
-            accessGroupe={true}
-            accessRanges={true}
-            accessVideos={true}
-        />
-        <CardPrice
-            title="Pack 20 heures"
-            price="900"
-            duration="1H à 2H / semaine"
-            gameplay="2H00 de coaching offertes"
-            feedback=""
-            accessDiscord={true}
-            accessGroupe={true}
-            accessRanges={true}
-            accessVideos={true}
-        />
-        <CardPrice
-            title="Pack découverte"
-            price="Gratuit"
-            duration="1H00"
-            gameplay="Venir avec un gameplay de 20 min en 2 tables"
-            feedback="Retour constructif sur les points à travailler"
-            accessDiscord={true}
-            accessGroupe={true}
-            accessRanges={true}
-            accessVideos={true}
-        />
-    </div>
-</div>
-);
+    return (
+        <div className="py-10 text-center">
+            <div className="py-10 max-w-4xl mx-auto">
+                <h1 className="text-4xl font-bold mb-3 pb-16">COACHING</h1>
+                <p className="mb-5">Voici un choix de coaching que je vous propose. Vous pouvez me poser des questions sur n’importe quelle offre, en m’écrivant dans contact ou via mes réseaux.</p>
+            </div>
+            <div className="flex flex-wrap justify-center"> 
+                <CardPrice />
+            </div>
+        </div>
+    );
 }
