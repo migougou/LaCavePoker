de45afd750107314db1b97db7d7e2e00b3b2dbcd
migougou <miguel.litche@espi.fr>
2024-08-25 17:37:38 +0200

    MAJ du component cardPrice. Mis en dynamique. et design de la page.

--- app/coaching/components/cardPrice.js
+++ app/coaching/components/cardPrice.js
@@ -1,6 +1,7 @@
 'use client';
 
 import useSupabaseTable from '../../hooks/useSupabaseTable';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 
 export default function CardPrice() {
     const { data: cardPrices, loading, error } = useSupabaseTable('CardPrice');
@@ -9,63 +10,71 @@ export default function CardPrice() {
     if (error) return <p>Erreur : {error}</p>;
 
     return (
-        <div className="flex flex-wrap justify-center gap-12 ">
+        <div className="flex flex-wrap justify-center gap-12">
             {cardPrices.map((card) => (
-                <div key={card.id} className="max-w-xs bg-white border border-black rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                    <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-white">{card.pack}</h5>
-                    <div className="flex items-baseline text-gray-900 dark:text-white">
-                        <span className="text-5xl font-extrabold tracking-tight">{card.price}</span>
-                        <span className="text-3xl font-semibold">€</span>
+                <div 
+                    key={card.id} 
+                    className={`w-full max-w-xs bg-white border border-black rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 flex flex-col ${
+                        card.pack === "Pack 20H" ? "bg-gradient-to-r from-purple-500 to-blue-500 text-white" : ""
+                    }`}
+                >
+                    {/* Titre de la carte */}
+                    <div className={`mb-4 text-xl font-medium ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-white"} h-12 flex items-center justify-center`}>
+                        {card.pack}
                     </div>
-                    <ul role="list" className="space-y-5 my-7">
-                        <li className="flex items-center">
-                            <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                            </svg>
-                            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{card.time}</span>
-                        </li>
-                        <li className="flex">
-                            <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                            </svg>
-                            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{card.line1}</span>
-                        </li>
-                        <li className="flex">
-                            <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                            </svg>
-                            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{card.line2}</span>
-                        </li>
+
+                    {/* Section du prix */}
+                    <div 
+                        className={`flex items-baseline ${card.pack === "Pack 20H" ? "text-white" : "text-gray-900 dark:text-white"} h-32 flex items-center justify-center`}
+                    >
+                        <span className={`text-5xl font-extrabold tracking-tight ${card.pack === "Pack Découverte" ? "text-xl" : ""}`}>
+                            {card.pack === "Pack Découverte" ? "GRATUIT. Viens découvrir une heure de coaching. Cette offre est unique et ne peut être renouvelée." : card.price}
+                        </span>
+                        {card.pack !== "Pack Découverte" && <span className="text-3xl font-semibold">€</span>}
+                    </div>
+
+                    {/* Contenu de la carte */}
+                    <ul role="list" className="space-y-6 my-7 flex-grow">
+                        {card.time && (
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.time}</span>
+                            </li>
+                        )}
+                        {card.line1 && (
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.line1}</span>
+                            </li>
+                        )}
+                        {card.line2 && (
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.line2}</span>
+                            </li>
+                        )}
                         {card.accessDiscord && (
-                            <li className="flex">
-                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                </svg>
-                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès à un discord privé</span>
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.accessDiscord}</span>
                             </li>
                         )}
                         {card.accessGroupe && (
-                            <li className="flex">
-                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                </svg>
-                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès à 2H de coaching groupé par mois</span>
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.accessGroupe}</span>
                             </li>
                         )}
                         {card.accessRanges && (
-                            <li className="flex">
-                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                </svg>
-                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux ranges</span>
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.accessRanges}</span>
                             </li>
                         )}
                         {card.accessVideos && (
-                            <li className="flex">
-                                <svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                </svg>
-                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Accès aux vidéos</span>
+                            <li className="flex items-start text-left">
+                                <CheckCircleIcon className={`flex-shrink-0 w-5 h-5 ${card.pack === "Pack 20H" ? "text-white" : "text-blue-700 dark:text-blue-500"} mt-0.5`} />
+                                <span className={`text-base font-normal leading-tight ${card.pack === "Pack 20H" ? "text-white" : "text-gray-500 dark:text-gray-400"} ms-3`}>{card.accessVideos}</span>
                             </li>
                         )}
                     </ul>
--- app/coaching/page.js
+++ app/coaching/page.js
@@ -6,7 +6,7 @@ export default function Coaching() {
         <div className="py-10 text-center">
             <div className="py-10 max-w-4xl mx-auto">
                 <h1 className="text-4xl font-bold mb-3 pb-16">COACHING</h1>
-                <p className="mb-5">Voici un choix de coaching que je vous propose. Vous pouvez me poser des questions sur n’importe quelle offre, en m’écrivant dans contact ou via mes réseaux.</p>
+                <p className="pb-16">Voici un choix de coaching que je vous propose. Vous pouvez me poser des questions sur n’importe quelle offre, en m’écrivant dans contact ou via mes réseaux.</p>
             </div>
             <div className="flex flex-wrap justify-center"> 
                 <CardPrice />
