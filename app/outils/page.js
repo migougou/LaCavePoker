5191838172aae729e1d0b0410c1ba36438d83d30
migougou <miguel.litche@espi.fr>
2024-08-27 11:32:58 +0200

    push MEP en cours.

--- app/outils/page.js
+++ app/outils/page.js
@@ -39,10 +39,10 @@ export default function Outils() {
                     <a href={selectedTool.url} target="_blank" rel="noopener noreferrer">
                         <h2 className="text-3xl font-semibold mb-4 py-16 hover:text-blue-500 cursor-pointer">{selectedTool.name}</h2>
                     </a>
-                    <div className="flex justify-center items-start space-x-4 max-w-4xl mx-auto pb-20">
-                        <img src={selectedTool.imageURL} alt={selectedTool.name} className="w-full max-w-lg" />
-                        <div className="w-1/2">
-                            <p>{selectedTool.description}</p>
+                    <div className="flex justify-center items-start space-x-8 max-w-full mx-auto pb-20">
+                        <img src={selectedTool.imageURL} alt={selectedTool.name} className="w-full max-w-3xl object-contain" />
+                        <div className="max-w-xl text-left">
+                            <p className="leading-relaxed text-lg" style={{ whiteSpace: 'pre-line' }}>{selectedTool.description}</p>
                         </div>
                     </div>
                 </>
