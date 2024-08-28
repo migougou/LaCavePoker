9a012a363cf96ce3bcc766e3d7029289fa3cfb1e
migougou <miguel.litche@espi.fr>
2024-08-28 14:30:39 +0200

    MEP du component QuiSuisJe. Ajustement de l'image et du texte. Mis en responsive

--- app/aPropos/components/QuiSuisJe.js
+++ app/aPropos/components/QuiSuisJe.js
@@ -1,31 +1,32 @@
 "use client"
-import { useState, useEffect } from 'react';
+
 import useSupabaseTable from '../../hooks/useSupabaseTable';
 
 export default function QuiSuisJe() {
-  const [isClient, setIsClient] = useState(false);
   const { data: bioData, loading, error } = useSupabaseTable('Biographie');
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return null; // Ne rien rendre côté serveur
-
   if (loading) return <div>Chargement...</div>;
-  if (error) return <div>Erreur : {error.message}</div>;
+  if (error) return <div>Erreur : {error}</div>;
 
   return (
     <div className="flex flex-col items-center text-center py-10">
       <h1 className="text-4xl font-bold mb-4 w-full pt-16">QUI SUIS-JE ?</h1>
-      {bioData && bioData.map((bio) => (
-        <div key={bio.id}>
-          <div className="flex flex-col md:flex-row items-center w-full">
-            <img src={bio.imageURL} alt="Photo de profil" className="w-full md:w-1/2 lg:w-1/3 p-4 max-w-xs md:max-w-none" />
-            <p className="w-full md:w-1/2 lg:w-2/3 p-4 mt-10 text-lg">{bio.bio}</p>
+      {bioData.map((bio) => (
+        <div 
+          key={bio.id} 
+          className="flex flex-col justify-center md:flex-row items-center w-full max-w-6xl mx-auto"
+        >
+          <img 
+            src={bio.imageURL} 
+            alt="Photo de profil" 
+            className="w-full md:w-1/3 lg:w-1/2 p-4 max-w-none" 
+          />
+          <div className="w-full md:w-2/3 lg:w-3/4 p-4 lg:mt-10 md:mt-0 flex justify-center">
+            <p className="text-base md:text-sm lg:text-lg max-w-md">{bio.bio}</p>
           </div>
         </div>
       ))}
     </div>
   );
 }
+
